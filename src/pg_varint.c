d311bb5a667d41e898ab0d09e3aeb45bcf8e5f01
sean <sean>
2012-10-13 21:35:48 +0000

    Bring the varint64 data type in to the world per [dead81f024], [4281ba3547] and [87f708eec9]. There are a few conversions and cast operators that I may need to add, but this should be a sufficient first release.

---
+++ src/pg_varint.c
@@ -0,0 +1,333 @@
+/*
+ * pg_varint.c
+ *
+ ******************************************************************************
+  This file contains routines that can be bound to a Postgres backend and
+  called by the backend in the process of processing queries.  The calling
+  format for these routines is dictated by Postgres architecture.
+******************************************************************************/
+
+#include "postgres.h"
+
+#include "fmgr.h"
+#include "libpq/pqformat.h" /* needed for send/recv functions */
+#include "utils/builtins.h" /* needed for pg_lltoa() */
+#include "utils/int8.h"
+
+#include "varint.h"
+
+#ifdef PG_MODULE_MAGIC
+PG_MODULE_MAGIC;
+#endif
+
+#define GETARG_VARINT64_P(x) PG_GETARG_POINTER(PG_GETARG_DATUM(x))
+
+/*
+ * Since we use V1 function calling convention, all these functions have
+ * the same signature as far as C is concerned.  We provide these prototypes
+ * just to forestall warnings when compiled with gcc -Wmissing-prototypes.
+ */
+extern Datum varint64_in(PG_FUNCTION_ARGS);
+extern Datum varint64_out(PG_FUNCTION_ARGS);
+extern Datum varint64_recv(PG_FUNCTION_ARGS);
+extern Datum varint64_send(PG_FUNCTION_ARGS);
+
+extern Datum varint64_lt(PG_FUNCTION_ARGS);
+extern Datum varint64_le(PG_FUNCTION_ARGS);
+extern Datum varint64_eq(PG_FUNCTION_ARGS);
+extern Datum varint64_ne(PG_FUNCTION_ARGS);
+extern Datum varint64_ge(PG_FUNCTION_ARGS);
+extern Datum varint64_gt(PG_FUNCTION_ARGS);
+extern Datum varint64_cmp(PG_FUNCTION_ARGS);
+
+extern Datum int2_to_varint64(PG_FUNCTION_ARGS);
+extern Datum int4_to_varint64(PG_FUNCTION_ARGS);
+extern Datum int8_to_varint64(PG_FUNCTION_ARGS);
+extern Datum varint64_to_int2(PG_FUNCTION_ARGS);
+extern Datum varint64_to_int4(PG_FUNCTION_ARGS);
+extern Datum varint64_to_int8(PG_FUNCTION_ARGS);
+
+extern Datum varint64_sizeof(PG_FUNCTION_ARGS);
+extern Datum varint64_sizeof2(PG_FUNCTION_ARGS);
+
+/* Notes:
+   struct varattrib_1b == good;
+ */
+
+/*****************************************************************************
+ * Helper functions
+ *****************************************************************************/
+
+static int64_t
+varlena_varint64_to_int64(const struct varlena* va) {
+  size_t consumed = 0;
+  int64_t num;
+
+  varint_to_int64(VARDATA_ANY(va), VARSIZE_ANY(va), &num, &consumed);
+  if (consumed == 0) {
+    ereport(ERROR, (errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
+                    errmsg("value is out of range for type VARINT64")));
+  }
+  return num;
+}
+
+
+static int64_t
+int64_to_varlena_varint64(const int64_t in) {
+  const size_t bufsz = VARINT64_MAX_BYTES;
+  char buf[VARINT64_MAX_BYTES];
+  size_t buflen = 0;
+  char *out;
+
+  int64_to_varint(in, bufsz, buf, &buflen);
+  if (buflen == 0) {
+    ereport(ERROR, (errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
+                    errmsg("value is out of range for type VARINT64")));
+  }
+  out = (char*)palloc(buflen + VARHDRSZ);
+  SET_VARSIZE(out, buflen + VARHDRSZ);
+  memcpy(VARDATA_ANY(out), buf, buflen);
+  PG_RETURN_POINTER(out);
+}
+
+
+/*****************************************************************************
+ * Input/Output functions
+ *****************************************************************************/
+
+PG_FUNCTION_INFO_V1(varint64_in);
+Datum
+varint64_in(PG_FUNCTION_ARGS) {
+  char *str = PG_GETARG_CSTRING(0);
+  int64 num;
+
+  (void)scanint8(str, false, &num);
+  return int64_to_varlena_varint64(num);
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_out);
+Datum
+varint64_out(PG_FUNCTION_ARGS) {
+  struct varlena *varint64 = PG_GETARG_VARLENA_PP(0);
+  size_t consumed = 0;
+  int64_t num;
+
+  varint_to_int64(VARDATA_ANY(varint64), VARSIZE_ANY(varint64), &num, &consumed);
+  if (consumed == 0) {
+    ereport(ERROR, (errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
+                    errmsg("value is out of range for type VARINT64")));
+  }
+
+  return DirectFunctionCall1(int8out, num);
+}
+
+
+/*****************************************************************************
+ * Operator class for defining B-tree index
+ *
+ * It's essential that the comparison operators and support function for a
+ * B-tree index opclass always agree on the relative ordering of any two data
+ * values.  Experience has shown that it's depressingly easy to write
+ * unintentionally inconsistent functions.  One way to reduce the odds of
+ * making a mistake is to make all the functions simple wrappers around an
+ * internal three-way-comparison function, as we do here.
+ *****************************************************************************/
+
+static int
+varint64_cmp_internal(const int64_t a, const int64_t b) {
+  if (a < b)
+    return -1;
+  if (a > b)
+    return 1;
+  return 0;
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_lt);
+Datum
+varint64_lt(PG_FUNCTION_ARGS) {
+  struct varlena *lhs_var = PG_GETARG_VARLENA_PP(0);
+  struct varlena *rhs_var = PG_GETARG_VARLENA_PP(1);
+
+  int64_t lhs = varlena_varint64_to_int64(lhs_var);
+  int64_t rhs = varlena_varint64_to_int64(rhs_var);
+
+  PG_RETURN_BOOL(varint64_cmp_internal(lhs, rhs) < 0);
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_le);
+Datum
+varint64_le(PG_FUNCTION_ARGS) {
+  struct varlena *lhs_var = PG_GETARG_VARLENA_PP(0);
+  struct varlena *rhs_var = PG_GETARG_VARLENA_PP(1);
+
+  int64_t lhs = varlena_varint64_to_int64(lhs_var);
+  int64_t rhs = varlena_varint64_to_int64(rhs_var);
+
+  PG_RETURN_BOOL(varint64_cmp_internal(lhs, rhs) <= 0);
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_eq);
+Datum
+varint64_eq(PG_FUNCTION_ARGS) {
+  struct varlena *lhs_var = PG_GETARG_VARLENA_PP(0);
+  struct varlena *rhs_var = PG_GETARG_VARLENA_PP(1);
+
+  int64_t lhs = varlena_varint64_to_int64(lhs_var);
+  int64_t rhs = varlena_varint64_to_int64(rhs_var);
+
+  PG_RETURN_BOOL(varint64_cmp_internal(lhs, rhs) == 0);
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_ne);
+Datum
+varint64_ne(PG_FUNCTION_ARGS) {
+  struct varlena *lhs_var = PG_GETARG_VARLENA_PP(0);
+  struct varlena *rhs_var = PG_GETARG_VARLENA_PP(1);
+
+  int64_t lhs = varlena_varint64_to_int64(lhs_var);
+  int64_t rhs = varlena_varint64_to_int64(rhs_var);
+
+  PG_RETURN_BOOL(varint64_cmp_internal(lhs, rhs) != 0);
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_ge);
+Datum
+varint64_ge(PG_FUNCTION_ARGS) {
+  struct varlena *lhs_var = PG_GETARG_VARLENA_PP(0);
+  struct varlena *rhs_var = PG_GETARG_VARLENA_PP(1);
+
+  int64_t lhs = varlena_varint64_to_int64(lhs_var);
+  int64_t rhs = varlena_varint64_to_int64(rhs_var);
+
+  PG_RETURN_BOOL(varint64_cmp_internal(lhs, rhs) >= 0);
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_gt);
+Datum
+varint64_gt(PG_FUNCTION_ARGS) {
+  struct varlena *lhs_var = PG_GETARG_VARLENA_PP(0);
+  struct varlena *rhs_var = PG_GETARG_VARLENA_PP(1);
+
+  int64_t lhs = varlena_varint64_to_int64(lhs_var);
+  int64_t rhs = varlena_varint64_to_int64(rhs_var);
+
+  PG_RETURN_BOOL(varint64_cmp_internal(lhs, rhs) > 0);
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_cmp);
+Datum
+varint64_cmp(PG_FUNCTION_ARGS) {
+  struct varlena *lhs_var = PG_GETARG_VARLENA_PP(0);
+  struct varlena *rhs_var = PG_GETARG_VARLENA_PP(1);
+
+  int64_t lhs = varlena_varint64_to_int64(lhs_var);
+  int64_t rhs = varlena_varint64_to_int64(rhs_var);
+
+  PG_RETURN_INT32(varint64_cmp_internal(lhs, rhs));
+}
+
+
+/*
+ * Conversion operators
+ */
+PG_FUNCTION_INFO_V1(int2_to_varint64);
+Datum
+int2_to_varint64(PG_FUNCTION_ARGS) {
+  int16 in = PG_GETARG_INT16(0);
+
+  return int64_to_varlena_varint64((int64_t)in);
+}
+
+
+PG_FUNCTION_INFO_V1(int4_to_varint64);
+Datum
+int4_to_varint64(PG_FUNCTION_ARGS) {
+  int32 in = PG_GETARG_INT32(0);
+
+  return int64_to_varlena_varint64((int64_t)in);
+}
+
+
+PG_FUNCTION_INFO_V1(int8_to_varint64);
+Datum
+int8_to_varint64(PG_FUNCTION_ARGS) {
+  int64 in = PG_GETARG_INT64(0);
+
+  return int64_to_varlena_varint64((int64_t)in);
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_to_int2);
+Datum
+varint64_to_int2(PG_FUNCTION_ARGS) {
+  struct varlena *var = PG_GETARG_VARLENA_PP(0);
+
+  int64_t num = varlena_varint64_to_int64(var);
+  int16 result = (int16)num;
+  if ((int64)result != num) {
+    ereport(ERROR,
+            (errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
+             errmsg("integer out of range")));
+  }
+
+  PG_RETURN_INT16((int16) result);
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_to_int4);
+Datum
+varint64_to_int4(PG_FUNCTION_ARGS) {
+  struct varlena *var = PG_GETARG_VARLENA_PP(0);
+
+  int64_t num = varlena_varint64_to_int64(var);
+  int32 result = (int32)num;
+  if ((int64)result != num) {
+    ereport(ERROR,
+            (errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
+             errmsg("integer out of range")));
+  }
+
+  PG_RETURN_INT32((int32) result);
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_to_int8);
+Datum
+varint64_to_int8(PG_FUNCTION_ARGS) {
+  struct varlena *var = PG_GETARG_VARLENA_PP(0);
+
+  PG_RETURN_INT64((int64) varlena_varint64_to_int64(var));
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_sizeof);
+Datum
+varint64_sizeof(PG_FUNCTION_ARGS) {
+  struct varlena *var = PG_GETARG_VARLENA_PP(0);
+
+  PG_RETURN_INT32(VARSIZE_ANY(var));
+}
+
+
+PG_FUNCTION_INFO_V1(varint64_sizeof2);
+Datum
+varint64_sizeof2(PG_FUNCTION_ARGS) {
+  struct varlena *var = PG_GETARG_VARLENA_PP(0);
+  bool include_varlena = PG_GETARG_BOOL(1);
+
+  if (include_varlena)
+    PG_RETURN_INT32(VARSIZE_ANY(var));
+
+  if (VARATT_IS_SHORT(var))
+    PG_RETURN_INT32(VARSIZE_ANY(var) - 1 /* 1 byte VARLENA header */);
+  else
+    PG_RETURN_INT32(VARSIZE_ANY(var) - 4 /* 4 byte VARLENA header */);
+}
---
+++ src/varint--1.0.sql
@@ -0,0 +1,144 @@
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION varint" to load this file. \quit
+
+CREATE FUNCTION varint64_in(cstring)
+   RETURNS varint64
+   AS 'MODULE_PATHNAME'
+   LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64_in(cstring) IS 'Convert a cstring to varint64';
+
+CREATE FUNCTION varint64_out(varint64)
+   RETURNS cstring
+   AS 'MODULE_PATHNAME'
+   LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64_in(cstring) IS 'Convert a varint64 to cstring';
+
+-- CREATE FUNCTION varint64_recv(internal)
+--    RETURNS varint64
+--    AS 'MODULE_PATHNAME'
+--    LANGUAGE C IMMUTABLE STRICT;
+
+-- CREATE FUNCTION varint64_send(varint64)
+--    RETURNS bytea
+--    AS 'MODULE_PATHNAME'
+--    LANGUAGE C IMMUTABLE STRICT;
+
+CREATE TYPE varint64 (
+   internallength = VARIABLE,
+   input = varint64_in,
+   output = varint64_out,
+   alignment = int4,
+   storage = main
+);
+
+CREATE FUNCTION varint64_lt(varint64, varint64) RETURNS bool
+   AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64_lt(varint64, varint64) IS 'Returns true if a < b';
+
+CREATE FUNCTION varint64_le(varint64, varint64) RETURNS bool
+   AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64_le(varint64, varint64) IS 'Returns true if a <= b';
+
+CREATE FUNCTION varint64_eq(varint64, varint64) RETURNS bool
+   AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64_eq(varint64, varint64) IS 'Returns true if a = b';
+
+CREATE FUNCTION varint64_ne(varint64, varint64) RETURNS bool
+   AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64_ne(varint64, varint64) IS 'Returns true if a != b';
+
+CREATE FUNCTION varint64_ge(varint64, varint64) RETURNS bool
+   AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64_ge(varint64, varint64) IS 'Returns true if a >= b';
+
+CREATE FUNCTION varint64_gt(varint64, varint64) RETURNS bool
+   AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64_gt(varint64, varint64) IS 'Returns true if a > b';
+
+CREATE OPERATOR < (
+   leftarg = varint64, rightarg = varint64, procedure = varint64_lt,
+   commutator = > , negator = >= ,
+   restrict = scalarltsel, join = scalarltjoinsel
+);
+CREATE OPERATOR <= (
+   leftarg = varint64, rightarg = varint64, procedure = varint64_le,
+   commutator = >= , negator = > ,
+   restrict = scalarltsel, join = scalarltjoinsel
+);
+CREATE OPERATOR = (
+   leftarg = varint64, rightarg = varint64, procedure = varint64_eq,
+   commutator = = , negator = <> ,
+   restrict = eqsel, join = eqjoinsel
+);
+CREATE OPERATOR <> (
+   leftarg = varint64, rightarg = varint64, procedure = varint64_ne,
+   commutator = <> , negator = = ,
+   restrict = neqsel, join = neqjoinsel
+);
+CREATE OPERATOR >= (
+   leftarg = varint64, rightarg = varint64, procedure = varint64_ge,
+   commutator = <= , negator = < ,
+   restrict = scalargtsel, join = scalargtjoinsel
+);
+CREATE OPERATOR > (
+   leftarg = varint64, rightarg = varint64, procedure = varint64_gt,
+   commutator = < , negator = <= ,
+   restrict = scalargtsel, join = scalargtjoinsel
+);
+
+CREATE FUNCTION varint64_cmp(varint64, varint64) RETURNS int4
+   AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64_cmp(varint64, varint64) IS 'varint64 B-tree comparison operator';
+
+CREATE OPERATOR CLASS varint64_ops
+    DEFAULT FOR TYPE varint64 USING btree AS
+        OPERATOR        1       < ,
+        OPERATOR        2       <= ,
+        OPERATOR        3       = ,
+        OPERATOR        4       >= ,
+        OPERATOR        5       > ,
+        FUNCTION        1       varint64_cmp(varint64, varint64);
+
+
+CREATE FUNCTION varint64(int2) RETURNS varint64 AS 'MODULE_PATHNAME', 'int2_to_varint64'
+  LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64(int2) IS 'Convert a varint64 to an int2';
+
+CREATE FUNCTION varint64(int4) RETURNS varint64 AS 'MODULE_PATHNAME', 'int4_to_varint64'
+  LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64(int4) IS 'Convert a varint64 to an int4';
+
+CREATE FUNCTION varint64(int8) RETURNS varint64 AS 'MODULE_PATHNAME', 'int8_to_varint64'
+  LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION varint64(int8) IS 'Convert a varint64 to an int8';
+
+CREATE FUNCTION int2(varint64) RETURNS int2 AS 'MODULE_PATHNAME', 'varint64_to_int2'
+  LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION int2(varint64) IS 'Convert an int2 to varint64';
+
+CREATE FUNCTION int4(varint64) RETURNS int4 AS 'MODULE_PATHNAME', 'varint64_to_int4'
+  LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION int4(varint64) IS 'Convert an int4 to varint64';
+
+CREATE FUNCTION int8(varint64) RETURNS int8 AS 'MODULE_PATHNAME', 'varint64_to_int8'
+  LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION int8(varint64) IS 'Convert an int8 to varint64';
+
+CREATE CAST (int2 AS varint64) WITH FUNCTION varint64(int2) AS IMPLICIT;
+CREATE CAST (int4 AS varint64) WITH FUNCTION varint64(int4) AS IMPLICIT;
+CREATE CAST (int8 AS varint64) WITH FUNCTION varint64(int8) AS IMPLICIT;
+
+CREATE CAST (varint64 AS int2) WITH FUNCTION int2(varint64) AS IMPLICIT;
+CREATE CAST (varint64 AS int4) WITH FUNCTION int4(varint64) AS IMPLICIT;
+CREATE CAST (varint64 AS int8) WITH FUNCTION int8(varint64) AS IMPLICIT;
+
+-- Deliberately using a function name different than LENGTH because the
+-- semantic meaning of sizeof() is different than that of LENGTH (the size of
+-- a VARINT64 vs the length of the string representation, respectively).
+CREATE FUNCTION sizeof(varint64) RETURNS integer AS 'MODULE_PATHNAME', 'varint64_sizeof'
+  LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION sizeof(varint64) IS 'Returns the complete on-disk size of the varint64 (including VARLENA header)';
+
+CREATE FUNCTION sizeof(varint64, bool) RETURNS integer AS 'MODULE_PATHNAME', 'varint64_sizeof2'
+  LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION sizeof(varint64, bool) IS 'Returns the complete on-disk size of the varint64. When the extra argument is set, it includes the VARLENA header in the calculation.';
